42ef3607268c4d53fbeaa62cff0e4ce931bd18b2
vsilvanka <vsilvanska@francoisdesales.be>
2024-04-22 11:48:23 +0200

    tailwindcss marche

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js"
+  ],
   theme: {
     extend: {},
   },
