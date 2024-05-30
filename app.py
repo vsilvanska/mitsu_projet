4b52d014ca43ec1011f9fcc4bc769b91d82f94b3
vsilvanska <vsilvanska@francoisdesales.be>
2024-05-30 09:57:10 +0200

    edit

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask import session
+from flask import Flask, render_template, request, redirect, url_for, session
 import logging
 import csv
-import jinja2
 from email_validator import validate_email, EmailNotValidError
 
-
 def verif_email(email):
-	try:
-	# validate and get info
-		v = validate_email(email)
-		# replace with normalized form
-		print("True")
-	except EmailNotValidError as e:
-		# email is not valid, exception message is human-readable
-		print(str(e))
+    try:
+        v = validate_email(email)
+        print("True")
+    except EmailNotValidError as e:
+        print(str(e))
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
@@ -23,8 +17,8 @@ app.secret_key = b'bahe004cc8de79cc96482b95db2d75473a3aa855b3270350267ccc92bddd4
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 def index():
-    with open("data.csv", "r", encoding="utf-8") as fichier_csv:
-        data = list(csv.DictReader(fichier_csv, delimiter=";"))      
+    with open("data1.csv", "r", encoding="utf-8") as fichier_csv:
+        data = list(csv.DictReader(fichier_csv, delimiter=";"))
     return render_template('index.html', data=data)
 
 @app.route("/reg", methods=['GET', 'POST'])
@@ -37,17 +31,16 @@ def reg():
         session['email'] = request.form['email']
         session['password'] = request.form['password']
 
-
         new_id = None
 
-        with open("data.csv", "r", encoding="utf-8", newline="") as fichier_csv:
+        with open("data1.csv", "r", encoding="utf-8", newline="") as fichier_csv:
             data = list(csv.DictReader(fichier_csv, delimiter=";"))
             if len(data) == 0:
-                 new_id = 1
+                new_id = 1
             elif len(data) > 0:
                 new_id = int(data[-1]['id']) + 1  
 
-        with open("data.csv", "a", encoding="utf-8", newline="") as fichier_csv:                      
+        with open("data1.csv", "a", encoding="utf-8", newline="") as fichier_csv:
             writer = csv.writer(fichier_csv, delimiter=';')            
             line = [new_id, session['name'], session['email'], session['password']]
             writer.writerow(line)
@@ -57,7 +50,6 @@ def reg():
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
-
 
 @app.route("/events", methods=['GET', 'POST'])
 def events():
@@ -69,13 +61,12 @@ def events():
         session['date'] = request.form['date']
         session['time'] = request.form['time']
 
-    with open("data.csv", "a", encoding="utf-8", newline="") as fichier_csv:                      
-        writer = csv.writer(fichier_csv, delimiter=';')            
-        line = [session['title'], session['description'], session['date'], session['time'] ]
-        writer.writerow(line)
+        with open("data1.csv", "a", encoding="utf-8", newline="") as fichier_csv:
+            writer = csv.writer(fichier_csv, delimiter=';')            
+            line = [session['title'], session['description'], session['date'], session['time']]
+            writer.writerow(line)
 
-    return redirect('/events')
-
-
+        return redirect('/events')
+        
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
--- app1.py
+++ app1.py
@@ -1,25 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-import logging
+from flask import Flask, render_template, request, redirect, url_for
+from flask import session
 import csv
-from email_validator import validate_email, EmailNotValidError
-
-def verif_email(email):
-    try:
-        v = validate_email(email)
-        print("True")
-    except EmailNotValidError as e:
-        print(str(e))
 
 app = Flask(__name__)
-logger = logging.getLogger(__name__)
-app.secret_key = b'bahe004cc8de79cc96482b95db2d75473a3aa855b3270350267ccc92bddd46c5'
+app.secret_key = b'bafe004cc8de79cc96482b95db2d75473a3aa855b3270350267ccc92bddd46c5'
 
 @app.route('/', methods=['GET'])
-@app.route("/index1", methods=['GET'])
-def index():    
+@app.route("/index", methods=['GET'])
+def index():  
+    return render_template('index1.html')
+
+
+@app.route("/table", methods=['GET'])
+def table():    
     with open("data1.csv", "r", encoding="utf-8") as fichier_csv:
         data = list(csv.DictReader(fichier_csv, delimiter=";"))      
-    return render_template('index1.html', data=data)
+    return render_template('table.html', data=data)
 
 
 @app.route("/encode", methods=['GET', 'POST'])
@@ -28,9 +24,9 @@ def encode():
         return render_template('encode.html')
 
     elif request.method == 'POST':
-        session['first_name'] = request.form['first_name']
-        session['last_name'] = request.form['last_name']
-        session['email'] = request.form['email']        
+        session['title'] = request.form['title']
+        session['date'] = request.form['date']
+        session['time'] = request.form['time']        
         new_id = None
 
         with open("data1.csv", "r", encoding="utf-8", newline="") as fichier_csv:
@@ -42,7 +38,7 @@ def encode():
 
         with open("data1.csv", "a", encoding="utf-8", newline="") as fichier_csv:                      
             writer = csv.writer(fichier_csv, delimiter=';')            
-            line = [new_id, session['first_name'], session['last_name'], session['email']]
+            line = [new_id, session['title'], session['date'], session['time']]
             writer.writerow(line)
         
         return redirect('/submitted')
@@ -50,8 +46,8 @@ def encode():
 @app.route('/submitted')
 def submitted():
     return render_template('submitted.html',
-                           first_name=session['first_name'],
-                           last_name=session['last_name'],
+                           title=session['title'],
+                           date=session['date'],
                            redirect=url_for('table'),
                            delay=5000,
                            )
@@ -63,35 +59,38 @@ def edit(id):
         with open("data1.csv", "r", encoding="utf-8", newline="") as fichier_csv:
             data = list(csv.DictReader(fichier_csv, delimiter=";"))
 
+            # Rechercher une ligne par son ID et modifier son contenu
             for line in data:
                 if line['id'] == id:
-                    prenom = line['prenom']
-                    nom =  line['nom']
-                    email = line['email']
+                    title = line[title]
+                    date =  line[date]
+                    time = line['time']
 
         return render_template('edit.html',
-                                prenom = prenom,
-                                nom = nom,
-                                email = email
+                                title = title,
+                                date = date,
+                                time = time
                                 )
      
     elif request.method == 'POST':
-        session['first_name'] = request.form['first_name']
-        session['last_name'] = request.form['last_name']
-        session['email'] = request.form['email']
+        session['title'] = request.form['title']
+        session['date'] = request.form['date']
+        session['time'] = request.form['time']
         data = []
 
         with open("data1.csv", "r", encoding="utf-8", newline="") as fichier_csv:
             data = list(csv.DictReader(fichier_csv, delimiter=";"))
 
+        # Rechercher une ligne par son ID et modifier son contenu
         for line in data:
             if line['id'] == id:
-                line['prenom'] = session['first_name']
-                line['nom'] = session['last_name']
-                line['email'] = session['email']
+                line[title] = session['title']
+                line[date] = session['date']
+                line['time'] = session['time']
 
+        # Réécrire le fichier CSV avec les modifications
         with open('data1.csv', mode='w', newline='') as file:
-            fieldnames = ['id', 'prenom', 'nom', 'email']
+            fieldnames = ['id', 'title', 'date', 'time']
             writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=";")
             writer.writeheader()
             writer.writerows(data)
@@ -106,12 +105,15 @@ def delete(id):
     with open("data1.csv", "r", encoding="utf-8", newline="") as fichier_csv:
         data = list(csv.DictReader(fichier_csv, delimiter=";"))
 
+    # Search for the dictionary that contains the line to be deleted
     for line in data:
         if line['id'] == id:
+            # Remove the dictionary from the list
             data.remove(line)
 
+    # Write the modified list of dictionaries to the CSV file
     with open('data1.csv', mode='w', newline='') as file:
-        fieldnames = ['id', 'prenom', 'nom', 'email']
+        fieldnames = ['id', 'title', 'date', 'time']
         writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=";")
         writer.writeheader()
         writer.writerows(data)
